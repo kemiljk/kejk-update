a5ebdd4ccab280aed6028ec1d2916e7fb8366ee7
Karl Koch <karl@kejk.tech>
2022-03-05 19:04:34 +0000

    fix: swap blur

--- components/Nav.vue
+++ components/Nav.vue
@@ -4,7 +4,7 @@
 			class="background-blur fixed bottom-0 z-50 mx-auto h-16 w-full flex-none pl-4 backdrop-blur-sm duration-500 sm:pl-6 md:top-0 lg:px-8"
 		>
 			<div
-				class="md:overflow-none scrollbar-hidden flex h-16 w-full items-center justify-between overflow-x-auto"
+				class="md:overflow-none scrollbar-hidden background-blur flex h-16 w-full items-center justify-between overflow-x-auto"
 			>
 				<Logo class="mr-6 shrink-0 lg:mr-0" />
 				<div
