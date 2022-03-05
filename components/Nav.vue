3d514cdf44499f4f59b977a224e522c7e34d53c6
Karl <karl@kejk.tech>
2022-03-05 22:02:22 +0000

    fix: nav padding

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
 	<nav>
 		<div
-			class="background-blur fixed bottom-0 z-50 mx-auto h-16 w-full flex-none pl-4 backdrop-blur-sm duration-500 sm:pl-6 md:top-0 lg:px-8"
+			class="background-blur fixed bottom-0 z-50 mx-auto h-16 w-full flex-none backdrop-blur-sm duration-500 md:top-0"
 		>
 			<div
 				class="md:overflow-none scrollbar-hidden background-blur flex h-16 w-full items-center justify-between overflow-x-auto"
 			>
-				<Logo class="mr-6 shrink-0 lg:mr-0" />
+				<Logo class="mx-6 shrink-0" />
 				<div
-					class="mr-6 flex shrink-0 items-center justify-between space-x-2 md:space-x-4 lg:mr-0"
+					class="pr-6 flex shrink-0 items-center justify-between space-x-2 md:space-x-4"
 				>
 					<slot></slot>
 				</div>
