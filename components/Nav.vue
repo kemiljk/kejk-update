4dd1026cd5cfb0ffb7366f2d639c9d8cb8327c46
Karl Koch <karl@kejk.tech>
2022-03-05 17:39:37 +0000

    fix: desktop is nav top

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,9 +1,7 @@
 <template>
-	<nav
-		class="supports-backdrop-blur:bg-white/95 supports-backdrop-blur:dark:bg-black/95 fixed bottom-0 z-50 mx-auto h-16 w-full bg-white dark:bg-black"
-	>
+	<nav>
 		<div
-			class="background-blur fixed bottom-0 z-50 mx-auto w-full flex-none pl-4 duration-500 sm:pl-6 lg:px-8"
+			class="background-blur fixed bottom-0 z-50 mx-auto h-16 w-full flex-none pl-4 duration-500 sm:pl-6 md:top-0 lg:px-8"
 		>
 			<div
 				class="md:overflow-none scrollbar-hidden flex h-16 w-full items-center justify-between overflow-x-auto"
