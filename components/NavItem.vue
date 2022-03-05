2c91e3f4c664993e87751fd8bc99e2205435126e
Karl <karl@kejk.tech>
2022-03-05 22:21:21 +0000

    fix

--- components/NavItem.vue
+++ components/NavItem.vue
@@ -4,7 +4,7 @@
       <a
         :href="`${href}`"
         target="_blank"
-        class="flex w-max items-center justify-center rounded-full border border-neutral-300 bg-neutral-200 px-4 py-2 font-normal text-neutral-900 no-underline transition duration-200 ease-in-out dark:border-neutral-700 dark:bg-neutral-700 dark:text-white md:block md:hover:bg-neutral-200 md:hover:text-black md:dark:hover:bg-neutral-800 md:dark:hover:text-white"
+        class="flex w-max items-center justify-center rounded-full border border-neutral-300 bg-neutral-200 px-4 py-2 font-normal text-neutral-900 no-underline transition duration-200 ease-in-out dark:border-neutral-700 dark:bg-neutral-800 dark:text-white md:block md:hover:bg-neutral-200 md:hover:text-black md:dark:hover:bg-neutral-800 md:dark:hover:text-white"
       >
         <slot />
       </a>
@@ -12,7 +12,7 @@
     <div v-else>
       <NuxtLink
         :to="`${to}`"
-        class="flex w-max items-center justify-center rounded-full border border-neutral-300 bg-neutral-200 px-4 py-2 font-normal text-neutral-900 no-underline transition duration-200 ease-in-out dark:border-neutral-700 dark:bg-neutral-700 dark:text-white md:block md:hover:bg-neutral-200 md:hover:text-black md:dark:hover:bg-neutral-800 md:dark:hover:text-white"
+        class="flex w-max items-center justify-center rounded-full border border-neutral-300 bg-neutral-200 px-4 py-2 font-normal text-neutral-900 no-underline transition duration-200 ease-in-out dark:border-neutral-700 dark:bg-neutral-800 dark:text-white md:block md:hover:bg-neutral-200 md:hover:text-black md:dark:hover:bg-neutral-800 md:dark:hover:text-white"
         :class="[
           {
             'border-2 border-teal-500 bg-neutral-300 text-neutral-900 dark:border-teal-500 dark:bg-neutral-700 dark:text-white md:hover:bg-neutral-400 md:dark:hover:bg-neutral-600':
