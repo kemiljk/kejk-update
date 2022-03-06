af64c239ea1d649f84850befd2fc9f488ea0b4d8
Karl <karl@kejk.tech>
2022-03-06 21:17:29 +0000

    ring

--- components/NavItem.vue
+++ components/NavItem.vue
@@ -15,7 +15,7 @@
         class="flex w-max items-center justify-center rounded-full border border-neutral-300 bg-neutral-200 px-4 py-2 font-normal text-neutral-900 no-underline transition duration-200 ease-in-out dark:border-neutral-700 dark:bg-neutral-800 dark:text-white md:block md:hover:bg-neutral-200 md:hover:text-black md:dark:hover:bg-neutral-800 md:dark:hover:text-white"
         :class="[
           {
-            'border ring ring-teal-500 dark:ring-teal-500 border-teal-500 text-neutral-900 dark:border-teal-500 dark:text-white md:hover:bg-neutral-400 md:dark:hover:bg-neutral-600':
+            'border ring-1 ring-inset ring-teal-500 dark:ring-teal-500 border-teal-500 text-neutral-900 dark:border-teal-500 dark:text-white md:hover:bg-neutral-400 md:dark:hover:bg-neutral-600':
               isActive,
           },
         ]"
