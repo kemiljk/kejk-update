300cdaa64765689148ea5b5de019a20f6329e7b4
Karl <karl@kejk.tech>
2022-03-06 17:20:35 +0000

    nav border

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
 	<nav>
 		<div
-			class="bg-neutral-100/75 dark:bg-neutral-800/75 fixed bottom-0 z-50 mx-auto h-16 w-full flex-none duration-500 md:top-0"
+			class="bg-neutral-100/75 dark:bg-neutral-800/75 fixed bottom-0 z-50 mx-auto h-16 w-full flex-none duration-500 md:top-0 border-t border-neautral-300 dark:border-neutral-600 md:border-t-none"
 		>
 			<div
 				class="md:overflow-none scrollbar-hidden background-blur flex h-16 w-full items-center justify-between overflow-x-auto"
