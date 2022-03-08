824f74116442f8a1bf8de99e21404cda02953c42
Karl <karl@kejk.tech>
2022-03-08 07:44:09 +0000

    swap to Spline Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+        sans: ['"Spline Sans"', ...defaultTheme.fontFamily.sans],
         display: '"Space Grotesk"',
         mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
       },
